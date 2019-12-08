2de8dd3a925253667947e7b8ca06c0134c476c36
ocancela <ocancela@yahoo.com>
2019-12-08 09:52:32 -0300

    Volví a comentar una línea del midelware web

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -67,7 +67,7 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {dd($request);
       //$path = $post->image->store("public/avatar");
 
     //  $nombreArchivo = basename($path);
