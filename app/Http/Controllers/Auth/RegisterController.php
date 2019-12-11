a0d9d703ae5facd458e506980db791b6602f074b
Valentina Mammolino <vmammolino@gmail.com>
2019-12-11 10:54:41 -0300

    cambie redirect de REgister y toque css

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/home';
+       protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
