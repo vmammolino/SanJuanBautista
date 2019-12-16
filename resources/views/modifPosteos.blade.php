8065c5ce20dceb89834367ea340992051e6fceef
Valentina Mammolino <vmammolino@gmail.com>
2019-12-16 15:28:20 -0300

    actualice la vista de modificar posteos

--- resources/views/modifPosteos.blade.php
+++ resources/views/modifPosteos.blade.php
@@ -31,10 +31,11 @@
                               <div class="card-body">
                                   <form method="POST" action="/modifPosteos"  enctype="multipart/form-data">
                                       @csrf
-                                          <input id="id" type="hidden"  name="id" value="{{$posteo->id}}">
-                                          <input id="user_id" type="hidden"  name="user_id" value="{{$posteo->user_id}}">
+                                      <input id="id" type="hidden"  name="id" value="{{$posteo->id}}">
+                                      <input id="user_id" type="hidden"  name="user_id" value="{{$posteo->user_id}}">
                                       <div class="form-group row">
-                                          <label for="type_id" class="col-md-4 col-form-label text-md-right">{{ __('CATEGORIA') }}</label>
+                                        <label for="type_id" class="col-md-4 col-form-label text-md-right">{{ __('Categoría') }}</label>
+                                        <div class="col-md-6">
                                            <select name="type_id" id="type_id" >
                                             @switch($posteo->type_id)
                                             @case(1)
@@ -69,9 +70,10 @@
                                                   </span>
                                               @enderror
                                           </div>
+                                        </div>
 
                                       <div class="form-group row">
-                                          <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('TITULO') }}</label>
+                                          <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Título') }}</label>
                                           <div class="col-md-6">
                                             <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{$posteo->title }}" required autocomplete="title" autofocus>
                                               @error('title')
@@ -82,10 +84,10 @@
                                           </div>
                                       </div>
                                       <div class="form-group row">
-                                          <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('DESCRIPCIÓN') }}</label>
+                                          <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descripción') }}</label>
                                           <div class="col-md-6">
-                                            <textarea id="description" rows="4" cols="50" name="description"> {{$posteo->description}}
-
+                                            <textarea id="description" name="description" rows="5" class="field-text-area" name="description">
+                                              {{$posteo->description }}
                                             </textarea>
                                               @error('description')
                                                   <span class="invalid-feedback" role="alert">
@@ -95,7 +97,7 @@
                                           </div>
                                       </div>
                                       <div class="form-group row">
-                                          <label for="link" class="col-md-4 col-form-label text-md-right">{{ __('CONTACTO') }}</label>
+                                          <label for="link" class="col-md-4 col-form-label text-md-right">{{ __('Link o Contacto') }}</label>
 
                                           <div class="col-md-6">
                                               <input id="link" type="text" class="form-control @error('link') is-invalid @enderror" name="link" value="{{$posteo->link}}" required autocomplete="link" autofocus>
@@ -109,7 +111,7 @@
                                       </div>
 
                                       <div class="form-group row">
-                                          <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('IMAGEN') }}</label>
+                                          <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Imagen') }}</label>
                                             <img  src="/storage/posteo/{{$posteo->image}}" height="75" width="75" >
                                           <div class="col-md-6">
                                               <input id="image" type="file" class="form-control" name="image">
